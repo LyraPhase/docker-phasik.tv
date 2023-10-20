f4f229cf2bacf143621409e7893478e3dc5cb66f
James Cuzella <james.cuzella@lyraphase.com>
2023-10-20 13:24:19 -0600

    Remove old unused prototyping comment cruft

--- main.go
+++ main.go
@@ -40,7 +40,6 @@ func serveFiles(w http.ResponseWriter, r *http.Request) {
 // response2JSON is a helper function to convert HTTP status codes to JSON
 // JSON output is written to the provided io.PipeWriter
 func response2JSON(status uint16, in_r *io.PipeReader, out_w *io.PipeWriter) {
-	// func response2JSON(status uint16, in_r *io.PipeReader, out_w *http.ResponseWriter) {
 	response := httpMinimalResponse{
 		Status:     http.StatusText(int(status)),
 		StatusCode: int(status),
@@ -48,34 +47,21 @@ func response2JSON(status uint16, in_r *io.PipeReader, out_w *io.PipeWriter) {
 	}
 
 	json_enc := json.NewEncoder(out_w)
-	// json_enc := json.NewEncoder(*out_w)
 	err := json.NewDecoder(in_r).Decode(&(&response).Data)
 	if err != nil && err != io.EOF {
 		fmt.Printf("response2JSON error: %+v\n", err)
-		// out_w.CloseWithError(err)
 		json_enc.Encode(httpMinimalResponse{Status: http.StatusText(http.StatusInternalServerError), StatusCode: int(http.StatusInternalServerError), Data: []byte(err.Error())})
 	}
 
-	// ok := json_enc.Encode(map[string]string{"status": "ok"})
 	json_enc.Encode(&response)
 	out_w.Close()
-	// *out_w.Close()
-	// out_w.Write()
-	// http.ResponseWriter.Write(*out_w, []byte{0})
-	// http.ResponseWriter.Write(*out_w, response.Data)
-	//Write([]byte("test"))
 	fmt.Printf("response2JSON: %+v\n", response)
 }
 
 // main is the entrypoint for the phasik.tv server
 func main() {
-
 	// logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
 
-	// const json_ok, var _err = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
-
-	// json_ok :=  // json.NewEncoder(json_pipe).Encode(map[string]string{"status": "ok"})
-	// json.NewEncoder()
 	fs := http.FileServer(http.Dir("/srv/www"))
 	http.Handle("/", fs)
 
@@ -86,21 +72,13 @@ func main() {
 	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
 		json_pipe_r, json_pipe_w := io.Pipe()
 		data_pipe_r, data_pipe_w := io.Pipe()
-		// fmt.Fprintf(w, "%s %s\n", r.Method, r.URL.Path)
-		// w.WriteHeader(http.StatusOK)
-		// { status: "ok" }
-		// json_ok := json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
-		// fmt.Fprintf(w, json_ok)
+
 		data_pipe_w.Close()
 		go response2JSON(http.StatusOK, data_pipe_r, json_pipe_w)
-		// go response2JSON(http.StatusOK, data_pipe_r, &w)
 
 		fmt.Printf("%s %s -> ", r.Method, r.URL.Path)
 
-		// var json_resp []byte
-		// _, err := json_pipe_r.Read(json_resp) // Copilot said: json_pipe_r.Read(json_resp) is not the correct way to read the response from the pipe.
 		json_resp, err := io.ReadAll(json_pipe_r)
-		// if err != nil && err != io.EOF {
 		if err != nil {
 			fmt.Println(err)
 		}
